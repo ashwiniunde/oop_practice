515a7077a12852ed3b92e86eddf5ddd802a85ae5
Ashwini Unde <146004602+ashwiniunde@users.noreply.github.com>
2025-01-23 23:37:50 +0530

    syntax.cpp

---
+++ Basic_c++/syntax.cpp
@@ -0,0 +1,8 @@
+#include <iostream> //#include <iostream> is a header file library that lets us work with input and output objects
+using namespace std;
+//using namespace std means that we can use names for objects and variables from the standard library
+
+int main() {
+  cout << "Hello World!";
+  return 0;
+}
