fc39f20eea77e2fe52f2d17c6487ca42769c2ca5
Ashwini Unde <146004602+ashwiniunde@users.noreply.github.com>
2025-01-23 23:41:12 +0530

    Add files via upload

---
+++ Basic_c++/cmath.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+    cout<<"Square root of 64 is "<<sqrt(64);
+    cout<<"Round 2.8 to the nearest of: "<<round(2.8);
+    cout<<"The natural log of 2 is : "<<log(2);
+    return 0;
+}
---
+++ Basic_c++/concatenation_string.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string firstName = "Ashwini";
+    string lastName = " Unde";
+    string fullName = firstName + lastName;
+    //The + operator can be used between strings to add them together to make a new string
+    cout << fullName;
+
+    
+}
