83bb23d6bef3631829e6ca6b196d81caf2743104
Ashwini Unde <146004602+ashwiniunde@users.noreply.github.com>
2025-01-23 23:51:35 +0530

    Add files via upload

---
+++ Basic_c++/If_Else.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x = 10;
+  int y = 20;
+  if (x > y) {
+    cout << "x is greater than y";
+  }  
+  else if(x < y){
+    cout<<"x is less than y";
+  }
+  else{
+  cout<<"x is equal to y";
+  }
+  return 0;
+}
