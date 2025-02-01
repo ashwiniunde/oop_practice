485be9194075a9b61472b64f75129a23312957e8
Ashwini Unde <146004602+ashwiniunde@users.noreply.github.com>
2025-02-01 12:33:32 +0530

    Access_Array_ELements.cpp

---
+++ Array/Access_Array_ELements.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{ 
+  //Accessing the element from the array
+    string arr[5]={"Apple","Mango","Banana","Orange","Chiku"};
+
+
+    cout<<arr[1];  //mango will be printed
+    cout<<arr[3];   //Chiku will be printed
+
+    /*
+    cout<<"Array Elements:";
+
+    for(int i=0;i<5;i++)
+    {
+        cout<<arr[i]<<endl;
+    }
+    cout<<"Access Element "<<arr[3];
+    */
+}
