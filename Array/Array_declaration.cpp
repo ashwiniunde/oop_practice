e891bd5c886daa2055559cb6fc05f83e939775f5
Ashwini Unde <146004602+ashwiniunde@users.noreply.github.com>
2025-02-01 07:54:39 +0530

    Array_declaration.cpp

---
+++ Array/Array_declaration.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int array[5] = {10, 20, 30, 40, 50};  // Declare an array with 5 elements
+
+    // Loop through the array
+    for (int i = 0; i < 5; i++)
+    {
+        cout << i << " ::: " << array[i] << endl;  // Print index and value
+    }
+
+
+
+
+
+    /*
+    int array[4];
+    array[0]=2;
+    array[1]=4;
+    array[2]=6;
+    array[3]=8;
+        // Accessing and printing elements of the array
+    cout << "arr[0]: " << array[0] << endl;
+    cout << "arr[1]: " << array[1] << endl;
+    cout << "arr[2]: " << array[2] << endl;
+    cout << "arr[3]: " << array[3] << endl;
+
+    return 0;
+    */
+  return 0;
+
+
+}
