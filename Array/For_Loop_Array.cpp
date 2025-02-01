acd7b5f560ca5439c73e7209b085a8155f0ccba8
Ashwini Unde <146004602+ashwiniunde@users.noreply.github.com>
2025-02-01 12:53:23 +0530

    For_Loop_Array.cpp

---
+++ Array/For_Loop_Array.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{   // Create an array of strings
+    string cars[5] = {"Volvo", "BMW", "Ford", "Mazda", "Tesla"};
+    for (int i=0;i<5;i++)   // Loop through strings
+    {
+        cout<<cars[i]<<endl;
+    }
+}
+/*
+OUTPUT : 
+
+Volvo
+BMW
+Ford
+Mazda
+Tesla
+
+*/
