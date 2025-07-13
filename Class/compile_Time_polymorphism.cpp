122910b8510eb082eab09f2d5063199cef27f824
Ashwini Unde <146004602+ashwiniunde@users.noreply.github.com>
2025-07-13 22:45:01 +0530

    Compile_Time_polymorphism.cpp

---
+++ Class/compile_Time_polymorphism.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+class Calculator {
+public:
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    float add(float a, float b) {
+        return a + b;
+    }
+
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+};
+
+int main() {
+    Calculator calc;
+
+    cout << "add(2, 3) = " << calc.add(2, 3) << endl;
+    cout << "add(2.5, 3.5) = " << calc.add(2.5f, 3.5f) << endl;
+    cout << "add(1, 2, 3) = " << calc.add(1, 2, 3) << endl;
+
+    return 0;
+}
