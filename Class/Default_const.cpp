1d610cbafc37da633ad3ba60b456122d77b9a06b
Ashwini Unde <146004602+ashwiniunde@users.noreply.github.com>
2025-07-13 21:43:33 +0530

    Default_const.cpp

---
+++ Class/Default_const.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+
+class student
+{
+    public:
+    // Default Constructor..
+    student()
+    {
+        cout<<" Default Constructor "<<endl;
+    }
+
+    
+};
+int main()
+{
+    student s;
+    return 0;
+}
