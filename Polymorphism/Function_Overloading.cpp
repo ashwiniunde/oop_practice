f5a13427b636b312132d8c38bb0e2ae648e92ea9
Ashwini Unde <146004602+ashwiniunde@users.noreply.github.com>
2025-07-24 09:17:30 +0530

    Function_Overloading.cpp

---
+++ Polymorphism/Function_Overloading.cpp
@@ -0,0 +1,30 @@
+// Function Overloading in Polymorphism 
+// same function name with different parameter
+#include<iostream>
+using namespace std;
+class A
+{
+    public:
+    void sayHello() // Fnunction  with no paramters
+    {
+        cout<<"Hello All !"<<endl;
+    }
+
+    void sayHello(string name) // function with 1 parameters
+    {
+        cout<<"Hello "<<name<<endl;
+    }
+
+    void sayHello(string name ,string name1) // Function with 2 parameters
+    {
+        cout<<"Hello "<<name<<" and "<<name1<<endl;
+    }
+
+};
+int main()
+{
+    A a;
+    a.sayHello();
+    a.sayHello("Ashwini");
+    a.sayHello("Ram","Sham");
+}
